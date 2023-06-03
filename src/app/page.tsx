15c2849937b607249ca1e6637fe6acb898b2ca14
WilkPrintes <wilkp90@gmail.com>
2023-06-03 12:58:33 -0300

    add main cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import {Home as Homeicon, Search, Library, ChevronLeft, ChevronRight, Play} from 'lucide-react'
+import { Home as Homeicon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -10,22 +10,22 @@ export default function Home() {
             <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
               <Homeicon />
               Home
-              </a>
+            </a>
             <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
               <Search />
               Search
-              </a>
+            </a>
             <a href="" className='flex items-center gap-3 text-sm font-semibold text-zinc-200'>
               <Library />
               Your Library
-              </a>
+            </a>
           </nav>
 
           <nav className='mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3'>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Liked Songs</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>The Girl Next Door</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Nelson Freire - Bach</a>
-              <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Blade Runner</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Liked Songs</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>The Girl Next Door</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Nelson Freire - Bach</a>
+            <a href="" className='text-sm text-zinc-400 hover:text-zinc-100'>Blade Runner</a>
           </nav>
 
         </aside>
@@ -39,53 +39,89 @@ export default function Home() {
             </button>
           </div>
 
-          <h1 className='font-semibold text-3xl mt-10' >Good Night</h1>
+          <h1 className='font-semibold text-3xl mt-10' >Good Morning</h1>
 
           <div className='grid grid-cols-3 gap-4 mt-4'>
-              <a href='' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
-                <Image src='/album.jpg' width={104} height={104} alt='Capa Evangelion Finaly'/>
-                <strong>Evangelion Finaly</strong>
-                <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
-                  <Play /> 
-                </button>
-              </a>
-              <a href='' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
-                <Image src='/album.jpg' width={104} height={104} alt='Capa Evangelion Finaly'/>
-                <strong>Evangelion Finaly</strong>
-                <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
-                  <Play /> 
-                </button>
-              </a>
-              <a href='' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
-                <Image src='/album.jpg' width={104} height={104} alt='Capa Evangelion Finaly'/>
-                <strong>Evangelion Finaly</strong>
-                <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
-                  <Play /> 
-                </button>
-              </a><a href='' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
-                <Image src='/album.jpg' width={104} height={104} alt='Capa Evangelion Finaly'/>
-                <strong>Evangelion Finaly</strong>
-                <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
-                  <Play /> 
-                </button>
-              </a><a href='' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
-                <Image src='/album.jpg' width={104} height={104} alt='Capa Evangelion Finaly'/>
-                <strong>Evangelion Finaly</strong>
-                <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
-                  <Play /> 
-                </button>
-              </a><a href='' className='bg-white/10 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/20 transition-colors'>
-                <Image src='/album.jpg' width={104} height={104} alt='Capa Evangelion Finaly'/>
-                <strong>Evangelion Finaly</strong>
-                <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
-                  <Play /> 
-                </button>
-              </a>
+            <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src='/album.jpg' width={104} height={104} alt='Capa Evangelion Finaly' />
+              <strong>Evangelion Finaly</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
+                <Play />
+              </button>
+            </a>
+            <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src='/album.jpg' width={104} height={104} alt='Capa Evangelion Finaly' />
+              <strong>Evangelion Finaly</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
+                <Play />
+              </button>
+            </a>
+            <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src='/album.jpg' width={104} height={104} alt='Capa Evangelion Finaly' />
+              <strong>Evangelion Finaly</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
+                <Play />
+              </button>
+            </a>
+            <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src='/album.jpg' width={104} height={104} alt='Capa Evangelion Finaly' />
+              <strong>Evangelion Finaly</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
+                <Play />
+              </button>
+            </a>
+            <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src='/album.jpg' width={104} height={104} alt='Capa Evangelion Finaly' />
+              <strong>Evangelion Finaly</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
+                <Play />
+              </button>
+            </a>
+            <a href='' className='bg-white/5 group rounded flex items-center gap-4 overflow-hidden hover:bg-white/10 transition-colors'>
+              <Image src='/album.jpg' width={104} height={104} alt='Capa Evangelion Finaly' />
+              <strong>Evangelion Finaly</strong>
+              <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-green-400 text-black ml-auto mr-8 invisible group-hover:visible '>
+                <Play />
+              </button>
+            </a>
           </div>
+
+          <h2 className='font-semibold text-2xl mt-10'>Made for Wilk Printes</h2>
+
+          <div className='grid grid-cols-8 gap-4 mt-4'>
+            <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src='/album.jpg' className='w-full' width={104} height={104} alt='Capa Evangelion Finaly' />
+              <strong className='font-semibold'>Evangelion Finaly</strong>
+              <span className='text-sm text-zinc-400' >Hideaki Anno</span>
+            </a>
+            <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src='/album.jpg' className='w-full' width={104} height={104} alt='Capa Evangelion Finaly' />
+              <strong className='font-semibold'>Evangelion Finaly</strong>
+              <span className='text-sm text-zinc-400' >Hideaki Anno</span>
+            </a>
+            <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src='/album.jpg' className='w-full' width={104} height={104} alt='Capa Evangelion Finaly' />
+              <strong className='font-semibold'>Evangelion Finaly</strong>
+              <span className='text-sm text-zinc-400' >Hideaki Anno</span>
+            </a>
+            <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src='/album.jpg' className='w-full' width={104} height={104} alt='Capa Evangelion Finaly' />
+              <strong className='font-semibold'>Evangelion Finaly</strong>
+              <span className='text-sm text-zinc-400' >Hideaki Anno</span>
+            </a>
+            <a href='' className='bg-white/5 p-3 rounded-md flex flex-col gap-2 hover:bg-white/10'>
+              <Image src='/album.jpg' className='w-full' width={104} height={104} alt='Capa Evangelion Finaly' />
+              <strong className='font-semibold'>Evangelion Finaly</strong>
+              <span className='text-sm text-zinc-400' >Hideaki Anno</span>
+            </a>
+          </div>
+
         </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">
-        footer
+        <div></div>
+        <div></div>
+        <div></div>
       </footer>
     </div>
   )
