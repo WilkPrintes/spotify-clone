5d2d66b5a17d2b8722bc874cf060143a9311fb58
WilkPrintes <wilkp90@gmail.com>
2023-06-05 00:16:36 -0300

    finish footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as Homeicon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat } from 'lucide-react'
+import { Home as Homeicon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat, Mic2, LayoutList, Laptop2, Volume2, Maximize2 } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -118,9 +118,9 @@ export default function Home() {
 
         </main>
       </div>
-      <footer className="bg-zinc-800 border-t border-zinc-700 p-6 flex items-center justify-between">
+      <footer className="bg-zinc-900 border-t border-zinc-700 px-6 py-4 flex items-center justify-between">
         <div className='flex items-center gap-3'>
-          <Image src='/album.jpg' className='w-full' width={32} height={32} alt='Capa Evangelion Finaly' />
+          <Image src='/album.jpg' width={64} height={64} alt='Capa Evangelion Finaly' />
           <div className='flex flex-col'>
             <strong className='font-normal'>Kom Susser Tod</strong>
             <span className='text-xs text-zinc-400'>Evangelion</span>
@@ -144,8 +144,19 @@ export default function Home() {
             <span className='text-xs text-zinc-400'>2:35</span>
           </div>
         </div>
-        <div>
-
+        <div className='flex items-center gap-2'>
+          <Mic2 size={20} />
+          <LayoutList size={20} />
+          <Laptop2 size={20} />
+          <div className='flex items-center gap-2'>
+            <Volume2 size={20} />
+            <div className='bg-white h-1 w-24 rounded-full bg-zinc-600'>
+              <div className='bg-green-400 h-1 w-1/3 rounded-full'></div>
+            </div>
+          </div>
+          <div>
+            <Maximize2 size={20} />
+          </div>
         </div>
       </footer>
     </div>
