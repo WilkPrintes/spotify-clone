e62f4c87499588e4f34da438e2da16c84e4fae82
WilkPrintes <wilkp90@gmail.com>
2023-06-05 00:07:44 -0300

    create mid footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Home as Homeicon, Search, Library, ChevronLeft, ChevronRight, Play } from 'lucide-react'
+import { Home as Homeicon, Search, Library, ChevronLeft, ChevronRight, Play, Shuffle, SkipBack, SkipForward, Repeat } from 'lucide-react'
 import Image from 'next/image'
 
 export default function Home() {
@@ -126,8 +126,23 @@ export default function Home() {
             <span className='text-xs text-zinc-400'>Evangelion</span>
           </div>
         </div>
-        <div>
-
+        <div className='flex flex-col items-center gap-2'>
+          <div className='flex items-center gap-6'>
+            <Shuffle className='text-zinc-200' />
+            <SkipBack className='text-zinc-200'/>
+            <button className='w-12 h-12 flex items-center justify-center pl-1 rounded-full bg-white text-black '>
+              <Play />
+            </button>
+            <SkipForward className='text-zinc-200'/>
+            <Repeat className='text-zinc-200'/>
+          </div>
+          <div className='flex items-center gap-2'>
+            <span className='text-xs text-zinc-400'>0:31</span>
+            <div className='bg-white h-1 w-96 rounded-full bg-zinc-600'>
+              <div className='bg-green-400 h-1 w-1/3 rounded-full'></div>
+            </div>
+            <span className='text-xs text-zinc-400'>2:35</span>
+          </div>
         </div>
         <div>
 
